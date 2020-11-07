568765228ad559c3971449b27f1a3ca85974ffb3
root <root@ZDBSurfacePro.localdomain>
2020-11-07 14:38:15 +0800

    更新忽略文件

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask,request
+from user.user_db import User
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return {
+        'status':True,
+        'message':'link success'
+    }
 
 
 if __name__ == '__main__':
